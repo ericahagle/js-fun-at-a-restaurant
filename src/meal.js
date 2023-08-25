24e3b488b227e58d7773d5536f79d6938eff6898
Erica Hagle <ericahagle.dev@gmail.com>
2023-08-25 14:33:35 -0700

    Add working functions to satisfy all tests in meal-test.js clear q git status

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,41 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+	return `Delicious ${menuItemName}`;
+};
 
-}
+function createMenuItem(name, price, type) {
+	var menuItem = {};
+	return menuItem = {name: name, price: price, type: type}; 
+};
 
+function addIngredients(ingredient, ingredients) {
+	if (!ingredients.includes(ingredient)) {
+		ingredients.push(ingredient);
+	};
+	return ingredients;
+};
+
+function formatPrice(price, formattedPrice) {
+	var formattedPrice = `$${price}`;
+	return formattedPrice;
+};
+
+function decreasePrice(price) {
+	var decreasedPrice = price * 0.9;
+	return decreasedPrice;
+};
+
+function createRecipe(title, ingredients, type) {
+	var recipe = {};
+	return recipe = {title: title, ingredients: ingredients, type: type};
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
+};
 
 
