cc0db2d988340e7a768550704e358b046abede5a
Erica Hagle <ericahagle.dev@gmail.com>
2023-08-26 16:48:57 -0700

    Add working functions to satisfy all tests in order-test.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+function takeOrder(order, deliveryOrders) {
+	if (deliveryOrders.length < 3) {
+		deliveryOrders.push(order);
+	};
+	return deliveryOrders;	
+};
+
+function refundOrder(orderNumber, deliveryOrders) {
+	for (var i = 0; i < deliveryOrders.length; i++) {
+		if (deliveryOrders[i].orderNumber === orderNumber) {
+			deliveryOrders.splice(i, 1);
+		};
+	};
+	return deliveryOrders;
+};
+
+function listItems(deliveryOrders) {
+	var itemNames = "";
+	for (var i = 0; i < deliveryOrders.length; i++) {
+		var item = deliveryOrders[i].item;
+		itemNames += item + ", ";
+	};
+	itemNames = itemNames.slice(0, -2);
+	return itemNames;
+};
+
+function searchOrder(deliveryOrders, item) {
+	for (var i = 0; i < deliveryOrders.length; i++) {
+		if (deliveryOrders[i].item === item) {
+			return true;
+		};
+	};
+	return false;
+};
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
-}
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
+};
